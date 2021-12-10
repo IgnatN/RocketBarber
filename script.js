c9ab73a75bea018534d63dcc9ffe67913d2019a5
IgnatN <84023038+IgnatN@users.noreply.github.com>
2021-12-10 11:07:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ class Block {
         this.img = img;
         this.title = title;
         this.text = text;
-        this.button = button
+        this.button = button;
     }
     showSlides() {
-        carousel.innerHTML += ` <div class="carousel-item image">
+        carousel.innerHTML += ` <div id="home" class="carousel-item">
                                 <img class="image_img" src="${this.img}" class="d-block w-100" alt="...">
-                                <div class="carousel-caption image_overlay">
+                                <div class="carousel-caption image">
                                 <h1>${this.title}</h1>
                                 <p>${this.text}</p>
                                 <button>${this.button}</button>
@@ -55,3 +55,84 @@ buton.forEach(btn => {
     btn.style.fontSize = '20px';
     btn.style.letterSpacing = '0.03em'
 })
+
+///// Second block , big numbers 
+
+let numbers = document.querySelector('.bigNumbers');
+class Numbers {
+    constructor(number, numberTitle, numberText) {
+        this.number = number;
+        this.numberTitle = numberTitle;
+        this.numberText = numberText
+    }
+    showNumbers() {
+        numbers.innerHTML += ` <div class="numberContent">
+                                 <img src="${this.number}" alt="">
+                                  <h5>${this.numberTitle}</h5>
+                                     <p>${this.numberText}</p>
+                                </div>`
+    }
+};
+let number1 = new Numbers('./images/number1.png', 'Почему мы?', 'Первоклассные мастера, вкусный кофеек и желаемый результат!')
+number1.showNumbers();
+let number2 = new Numbers('/images/number2.png', 'Забота', 'Только профессиональная машинка заиграет в руках нашего мастера.')
+number2.showNumbers();
+let number3 = new Numbers('/images/number3.png', 'Качество', 'Работы наших мастеров говорят сами за себя.')
+number3.showNumbers();
+let number4 = new Numbers('/images/number4.png', 'Современность', 'Стиль и качество только в Rocket Barbershop.')
+number4.showNumbers();
+
+//// Number Animation 
+
+let numb = document.querySelectorAll('.numberContent');
+
+window.addEventListener('scroll', () => {
+    numb[0].classList.add('active1');
+    numb[1].classList.add('active2');
+    numb[2].classList.add('active3');
+    numb[3].classList.add('active4');
+})
+
+////// OUR Team
+let ourTeam = document.querySelector('.teamBlocks');
+class Team {
+    constructor(photo, name, linkInst, instIcon) {
+        this.photo = photo;
+        this.name = name;
+        this.linkInst = linkInst;
+        this.instIcon = instIcon
+    }
+    showOurTeam() {
+        ourTeam.innerHTML += `<div class="blockTeam">
+        <div class="photoTeam"><a target="blanck" href="${this.linkInst}">
+        <img class="ourTeamImg" src="${this.photo}" alt="">
+        <img class="instIcon" src="${this.instIcon}"> </a></div>
+        <p>${this.name}</p>
+        </div>`
+    }
+}
+let empl1 = new Team('./images/ourTeam1.jpg', 'Raketa', 'https://www.instagram.com/raketa_barber', './images/inst-icon.png')
+empl1.showOurTeam();
+let empl2 = new Team('./images/ourTeam2.jpg', 'Raketka', 'https://www.instagram.com/anyaonoiku', './images/inst-icon.png')
+empl2.showOurTeam();
+let empl3 = new Team('./images/ourTeam3.jpg', 'Daria', 'https://www.instagram.com/dariaonoiku', './images/inst-icon.png')
+empl3.showOurTeam();
+let empl4 = new Team('./images/ourTeam4.jpg', 'Mirel', 'https://www.instagram.com/mirelpirgari', './images/inst-icon.png')
+empl4.showOurTeam();
+
+let title = document.querySelectorAll('h1');
+title.forEach(h1 => {
+    h1.style.width = 'auto';
+    h1.style.height = '53px';
+    h1.style.fontFamily = "Roboto Condensed";
+    h1.style.fontStyle = 'bold';
+    h1.style.fontWeight = '700';
+    h1.style.lineHeight = '53px';
+    h1.style.letterSpacing = '0.06em'
+    h1.style.marginTop = '4%'
+    h1.style.color = '#E6E6E6'
+});
+
+/////// Gallery
+let gallery = document.querySelector('.galleryBlocks');
+
